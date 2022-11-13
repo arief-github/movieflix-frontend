0e0bcada788d20916b756113691f5497aa9fc841
github.arief <78076941+arief-github@users.noreply.github.com>
2022-11-13 21:33:27 +0700

    first commit : add includes components, routes and services API using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// bootstrap component 
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+// our component
+import AddReview from './components/add-review';
+import MoviesList from './components/movies-list';
+import Movie from './components/movie';
+import Login from './components/login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+
+    async function login(user = null) {
+        // default user to null
+        setUser(user);
+    }
+
+    async function logout() {
+        setUser(null);
+    }
+
+
+    return (
+      <div>
+        <NavbarApp user={user}/> 
+
+        <Switch>
+          <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+          <Route path="/movies/:id/review" render={(props) => <AddReview {...props} user={user} />}></Route>
+          <Route path="/movies/:id" render={(props) => <Movie {...props} user={user} />}></Route>
+          <Route path="/login" render={(props) => <Login {...props} login={login}/>}></Route>
+        </Switch>   
+      </div>
+    );
+}
+
+function NavbarApp({ user }) {
+    return (
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">MERN-FLIX</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link>
+                  <Link to={"/movies"}>Movies</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  {
+                    user ? (
+                      <Link to={'/logout'}>Logout User</Link>
+                    ) : (
+                      <Link to={'/login'}>Login</Link>
+                    )
+                  }
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    )
 }
 
 export default App;
---
+++ src/components/movies-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MoviesList() {
+	return (
+		<div>
+			MoviesList
+		</div>
+	)
+}
+
+export default MoviesList;
