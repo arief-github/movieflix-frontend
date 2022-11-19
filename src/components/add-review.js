3cd016baab1cec472277c2eb23ad3148e6b7bef0
github.arief <78076941+arief-github@users.noreply.github.com>
2022-11-19 20:37:56 +0700

    add : feature add, edit and delete reviews

--- src/components/add-review.js
+++ src/components/add-review.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MovieDataService from '../services/movies';
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
-function AddReview() {
-	return (
-		<div>
-			Add Review
-		</div>
-	)
+const AddReview = (props) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { id } = useParams();
+
+    // if !user can't allow to perform adding review
+    useEffect(() => {
+        if (!props.user) {
+            navigate('/login');
+        }
+    });
+
+    const [editing, setEditing] = useState(false);
+    const [review, setReview] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    useEffect(() => {
+        if (location.state && location.state.currentReview) {
+            setEditing(true);
+            setReview(location.state.currentReview.review);
+        }
+    }, []);
+
+    const onChangeReview = (e) => {
+        setReview(e.target.value);
+    }
+
+    const saveReview = () => {
+        let data = {
+            review: review,
+            name: props.user.name,
+            user_id: props.user.id,
+            movie_id: id
+        }
+
+        if (editing) {
+            data.review_id = location.state.currentReview._id;
+            MovieDataService.updateReview(data)
+                .then((res) => { setSubmitted(true) })
+                .catch((e) => { console.log(e) })
+        } else {
+            MovieDataService.createReview(data)
+                .then((res) => { setSubmitted(true) })
+                .catch((e) => { console.log(e) })
+        }
+    }
+
+    return (
+    	<div>
+    		{submitted ? (
+    			<div style={{marginLeft: '20px'}}>
+    				<h4>Review Submitted!</h4>
+    				<Link to={"/movies/id/" + id}> Back to Movie </Link>
+    			</div>
+    		) : (
+    			<Form>
+    				<Form.Group>
+    					<Form.Label>{editing ? "Edit" : "Create"} Review</Form.Label>
+    					<Form.Control rows={10} as="textarea" required value = {review} onChange={onChangeReview}/>
+    				</Form.Group>
+    				<Button variant="primary" onClick={saveReview}> Submit </Button>
+    			</Form>
+    		)}
+    	</div>
+    )
 }
 
 export default AddReview;
--- src/components/movie.js
+++ src/components/movie.js
@@ -7,10 +7,10 @@ import moment from 'moment';
 import { Card, Container, Image, Col, Row, Button, Figure } from 'react-bootstrap';
 
 const Movie = (props) => {
-	const navigate = useNavigate();
-	const { id } = useParams();
+    const navigate = useNavigate();
+    const { id } = useParams();
     const [style, setStyle] = useState({
-      textDecoration: 'none'
+        textDecoration: 'none'
     });
 
     const [movie, setMovie] = useState({
@@ -35,59 +35,60 @@ const Movie = (props) => {
         getMovie(id)
     }, [id])
 
+    const deleteReview = (reviewId, index) => {
+        MovieDataService.deleteReview(reviewId, props.user.id).then((response) => {
+            setMovie((prevState) => {
+                prevState.reviews.splice(index, 1);
+                return({...prevState});
+            }).catch((e) => { console.log(e) });
+        })
+    }
+
     return (
         <div>
-        	<Container>
-        		<Row className="mt-4">
-        			<Col>
-        				<Figure>
+            <Container>
+                <Row className="mt-4">
+                    <Col>
+                        <Figure>
                             <Figure.Image src={movie.poster + "/100px250"} fluid/>            
                         </Figure>
-        			</Col>
-        			<Col>
-        				<Card>
-        					<Card.Header as = "h5">{movie.title}</Card.Header>
-        					<Card.Body>
-        						<Card.Text>
-        							{movie.plot}		
-        						</Card.Text>
-        						  {props.user && <Link style={style} onMouseOut={()=>setStyle({textDecoration:'none'})} onMouseOver={()=>setStyle({textDecoration:'underline'})} to={"/movies/id/" + id + "/review"}> Add Review </Link>}
-        					</Card.Body>
-        				</Card>
-        				<br></br>
-        				<h2> Reviews </h2>
-        				<br></br>
-        				{
-        					movie.reviews.map((review, index) => {
-        						return (
-        							<Card key={index}>
-        								<Card.Body>
-        									<h5>{review.name + " reviewed on "}{moment(review.date).format("Do MMMM YYYY")}</h5>
-        									<p>{review.review}</p>
-        									{
-        										props.user && props.user.id === review.user_id &&
-        										<Row>
-        											<Col>
-        												<Link to={{
-        													pathname: "/movies" + props.match.params.id + "/review",
-        													state: {currentReview: review}
-        												}}>
-        													Edit
-        												</Link>
-        											</Col>
-        											<Col>
-        												<Button variant="link">Delete</Button>
-        											</Col>
-        										</Row>
-        									}
-        								</Card.Body>
-        							</Card>
-        						)
-        					})
-        				}
-        			</Col>
-        		</Row>
-        	</Container>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Header as = "h5">{movie.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {movie.plot}        
+                                </Card.Text>
+                                  {props.user && <Link style={style} onMouseOut={()=>setStyle({textDecoration:'none'})} onMouseOver={()=>setStyle({textDecoration:'underline'})} to={"/movies/id/" + id + "/review"}> Add Review </Link>}
+                            </Card.Body>
+                        </Card>
+                        <br></br>
+                        <h2>Reviews</h2>
+                        <br />
+                        {movie.reviews.map((review, idx)=>{
+                          return (
+                            <Card style={{marginBottom:'0px', border:'none'}}>
+                              <Card.Body>
+                                  <h5>{review.name + " reviewed on " + moment(review.date).format('Do MMMM YYYY')}</h5>
+                                  <p>{review.review}</p>
+                                  {props.user && props.user.id === review.user_id.toString() &&
+                                    <Row>
+                                      <Col>
+                                        <span>
+                                          <Link className='UserActionLink' state={{currentReview:review}} to={"/movies/id/"+ id+"/review"}>Edit</Link>
+                                          <Link onClick={() => { deleteReview(review._id, idx) }} className='UserActionLink' style={{marginLeft: '10px'}}>Delete</Link>
+                                        </span>
+                                      </Col>
+                                    </Row>
+                                  }
+                              </Card.Body>
+                            </Card>
+                          )
+                        })}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
