7e293152130a8c510fe319e2e3034b9814950e35
github.arief <78076941+arief-github@users.noreply.github.com>
2022-11-21 21:13:03 +0700

    direct API from localhost to deploy backend

--- src/services/movies.js
+++ src/services/movies.js
@@ -2,29 +2,29 @@ import axios from 'axios';
 
 class MovieDataService {
     getAll(page = 0) {
-    	return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
+    	return axios.get(`https://guarded-savannah-47368.herokuapp.com/api/v1/movies?page=${page}`);
     }
 
     get(id) {
-    	return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+    	return axios.get(`https://guarded-savannah-47368.herokuapp.com/api/v1/movies/id/${id}`);
     }
 
     find(query, by = "title", page = 0) {
     	return axios.get(
-    		`http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+    		`https://guarded-savannah-47368.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`
     	);
     }
 
     createReview(data) {
-    	return axios.post("http://localhost:5000/api/v1/movies/review", data);
+    	return axios.post("https://guarded-savannah-47368.herokuapp.com/api/v1/movies/review", data);
     }
 
     updateReview(data) {
-    	return axios.put('http://localhost:5000/api/v1/movies/review', data);
+    	return axios.put('https://guarded-savannah-47368.herokuapp.com/api/v1/movies/review', data);
     }
 
     deleteReview(id, userId) {
-    	return axios.delete("http://localhost:5000/api/v1/movies/review", {
+    	return axios.delete("https://guarded-savannah-47368.herokuapp.com/v1/movies/review", {
     		data: {
     			review_id: id,
     			user_id: userId
@@ -32,7 +32,7 @@ class MovieDataService {
     	});
     }
     getRatings() {
-    	return axios.get("http://localhost:5000/api/v1/movies/ratings");
+    	return axios.get("https://guarded-savannah-47368.herokuapp.com/v1/movies/ratings");
     }
 }
 
